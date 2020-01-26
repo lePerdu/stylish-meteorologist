93416f8d4473551b95e0aa7a63a1dd82989f0433
lePerdu <zdpeltzer@gmail.com>
2020-01-26 08:45:33 -0500

    Return more specific info from clothing detection

--- camera/clothing.py
+++ camera/clothing.py
@@ -26,6 +26,12 @@ COAT_LABELS = [
 PANTS_LABELS = [
     'Pants',
     'Jeans',
+    'Slacks',
+]
+
+SHORTS_LABELS = [
+    'Short',
+    'Skirt',
 ]
 
 
@@ -37,18 +43,35 @@ def has_object_type(labels, clothing):
     return any((l['Name'] in clothing) for l in labels['Labels'])
 
 
+def has_person(labels):
+    return has_object_type(labels, PERSON_LABELS)
+
+
+def wearing_coat(labels):
+    return has_object_type(labels, COAT_LABELS)
+
+
+def wearing_pants(labels):
+    # Pants detection is a bit finicky, so also check for shorts and skirts
+    has_long = has_object_type(labels, PANTS_LABELS)
+    has_short = has_object_type(labels, SHORTS_LABELS)
+    # Bias towards having long pants if nothing is detected
+    # (the coat is kind of more important anyway)
+    return has_long or not has_short
+
+
 def wearing_warm_clothes(image_bytes):
+    """Gets a summary of the clothing detected in the image.
+    Returns:
+        Tuple of (wearing_coat, wearing_pants) or None if there is no person
+        detected in the image.
+    """
+
     labels = rekognition.detect_labels(
         Image={'Bytes': image_bytes},
         MinConfidence=MIN_CONFIDENCE)
 
-    for l in labels['Labels']:
-        print(f"{l['Confidence']}\t{l['Name']}")
-
-    if has_object_type(labels, PERSON_LABELS):
-        has_coat = has_object_type(labels, COAT_LABELS)
-        # has_pants = has_object_type(labels, PANTS_LABELS)
-
-        return has_coat
+    if has_person(labels):
+        return weaing_coat(labels), wearing_pants(labels)
     else:
-        raise DetectionError("No person detected")
+        return None
