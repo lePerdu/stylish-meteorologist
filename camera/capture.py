13589cc2ad6d236f60f1b830349413d3bcc11f3a
lePerdu <zdpeltzer@gmail.com>
2020-01-25 21:58:16 -0500

    Fix PiCamera imports

--- camera/capture.py
+++ camera/capture.py
@@ -1,12 +1,13 @@
 import io
+import time
 
-import PiCamera
+import picamera
 
 
 def capture_image():
-    camera = PiCamera()
+    camera = picamera.PiCamera()
     # Wait 2 seconds for the camera to adjsut to the conditions
-    camera.sleep(2)
+    time.sleep(2)
 
     camera.capture(buf)
     # TODO Is this okay? Will not closing the buffer have consequences?
